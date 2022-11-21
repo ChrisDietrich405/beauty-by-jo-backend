ba01cf9a282271ac97dc770740f4d9d65633f098
Julio <julioccpv@gmail.com>
2022-11-21 09:26:05 -0500

    Database structure updated

--- src/migrations/01-structure.sql
+++ src/migrations/01-structure.sql
@@ -1,45 +1,179 @@
--- @project SQL Beauty By Jo
--- @updated 2022-06-25
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 
-DROP TABLE IF EXISTS `user`;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `beautybyjo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table `password_reset`
+--
+
+CREATE TABLE `password_reset` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `hash` char(36) NOT NULL,
+  `expires_at` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `id` int(11) NOT NULL,
+  `specific_service_id` int(11) NOT NULL,
+  `date` timestamp NOT NULL,
+  `status` smallint(6) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table `service`
+--
+
+CREATE TABLE `service` (
+  `id` int(11) NOT NULL,
+  `status` smallint(6) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `path` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table `specific_service`
+--
+
+CREATE TABLE `specific_service` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `price` decimal(18,2) NOT NULL,
+  `status` smallint(6) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `time` int(11) NOT NULL DEFAULT '60',
+  `max_price` decimal(18,2) DEFAULT NULL,
+  `min_price` decimal(18,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table `user`
+--
+
 CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `IDX_e12875dfb3b1d92d7d7c5377e2` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `service`;
-CREATE TABLE `service` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `status` smallint NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `path` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+--
+-- Índices para tabelas despejadas
+--
 
-DROP TABLE IF EXISTS `specific_service`;
-CREATE TABLE `specific_service` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `price` decimal(18,2) NOT NULL,
-  `status` smallint NOT NULL,
-  `service_id` int NOT NULL,
-  `time` int NOT NULL DEFAULT '60',
-  PRIMARY KEY (`id`),
-  KEY `service_subservice_id_idx` (`service_id`),
-  CONSTRAINT `service_subservice_id` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+--
+-- Índices para tabela `password_reset`
+--
+ALTER TABLE `password_reset`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_password_reset_idx` (`user_id`);
 
-CREATE TABLE `schedule` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `specific_service_id` int NOT NULL,
-  `date` datetime NOT NULL,
-  `status` smallint NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `schedule_specific_service_fk_idx` (`specific_service_id`),
-  CONSTRAINT `schedule_specific_service_fk` FOREIGN KEY (`specific_service_id`) REFERENCES `specific_service` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+--
+-- Índices para tabela `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `schedule_specific_service_fk_idx` (`specific_service_id`),
+  ADD KEY `schedule_users_idx` (`user_id`);
+
+--
+-- Índices para tabela `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `specific_service`
+--
+ALTER TABLE `specific_service`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `service_subservice_id_idx` (`service_id`);
+
+--
+-- Índices para tabela `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `IDX_e12875dfb3b1d92d7d7c5377e2` (`email`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `password_reset`
+--
+ALTER TABLE `password_reset`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `service`
+--
+ALTER TABLE `service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `specific_service`
+--
+ALTER TABLE `specific_service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `password_reset`
+--
+ALTER TABLE `password_reset`
+  ADD CONSTRAINT `fk_user_password_reset` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Limitadores para a tabela `schedule`
+--
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `schedule_specific_service_fk` FOREIGN KEY (`specific_service_id`) REFERENCES `specific_service` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `schedule_users` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Limitadores para a tabela `specific_service`
+--
+ALTER TABLE `specific_service`
+  ADD CONSTRAINT `service_subservice_id` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`) ON DELETE CASCADE;
+COMMIT;
