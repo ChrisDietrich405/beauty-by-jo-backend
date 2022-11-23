ae47f35b70666954c1e082cc06ed1e516fd2e7c4
Chris <chrisdietrich3666@gmail.com>
2022-11-23 10:00:11 -0500

    fixing birthday

--- src/user/dto/create-user-dto.ts
+++ src/user/dto/create-user-dto.ts
@@ -1,14 +1,15 @@
-import { IsNotEmpty, IsEmail } from "class-validator"
+import { IsNotEmpty, IsEmail, IsDateString } from 'class-validator';
 
-
-export class CreateUserDto{
-    @IsNotEmpty()
-    firstName: string;
-    @IsNotEmpty() 
-    lastName: string;
-    @IsNotEmpty()
-    @IsEmail()
-    email: string; 
-    @IsNotEmpty() 
-    password: string; 
+export class CreateUserDto {
+  @IsNotEmpty()
+  firstName: string;
+  @IsNotEmpty()
+  lastName: string;
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+  @IsNotEmpty()
+  password: string;
+  @IsDateString()
+  birthday: string;
 }
